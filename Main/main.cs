bc627b9c9e21cd12948652a272d1e065e3c3449f
Preet Verma <2000vermapreet@gmail.com>
2025-01-15 23:54:07 +0530

    intro start

---
+++ Main/main.cs
@@ -0,0 +1,20 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+class MainProgram
+{
+   public static void Main(string[] args){
+    // primitive datatypes:
+//   number dataTypes in c sharp: int, long, uint, ulong
+     int number = 12;
+     long l = 4563;
+     uint r  = 22; //in this type of number we only save positive number greater than zero
+     Console.WriteLine("integer " + number);
+     Console.WriteLine("long number " + l);
+     Console.WriteLine("unsighned number " + r);
+     Console.WriteLine("long number " + l);
+     Console.WriteLine(int.MaxValue);
+    //  Console.Write
+   }
+}
